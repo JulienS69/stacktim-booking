116578f496ad3e74029ece91a3e133f19d67d9b0
j.seux@xefi.fr <j.seux@xefi.fr>
2024-02-22 13:34:13 +0100

    ⚡️ change card design ⚡️

--- lib/ui/dashboard/dashboard_view.dart
+++ lib/ui/dashboard/dashboard_view.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:glassmorphism/glassmorphism.dart';
 import 'package:stacktim_booking/helper/strings.dart';
 import 'package:stacktim_booking/ui/dashboard/dashboard_view_controller.dart';
 import 'package:stacktim_booking/widget/x_app_bar.dart';
@@ -33,35 +32,98 @@ class DashboardView extends GetView<DashboardViewController> {
                     shrinkWrap: true,
                     itemCount: controller.statusList.length,
                     itemBuilder: (context, index) {
-                      return Padding(
-                        padding: const EdgeInsets.only(bottom: 10.0),
-                        child: GlassmorphicContainer(
-                          width: 350,
-                          height: 80,
-                          borderRadius: 20,
-                          blur: 20,
-                          alignment: Alignment.bottomCenter,
-                          border: 2,
-                          linearGradient: LinearGradient(
-                              begin: Alignment.topLeft,
-                              end: Alignment.bottomRight,
-                              colors: [
-                                Colors.black.withOpacity(0.1),
-                                controller.getColorsByStatusTag(
-                                    statusTag: controller
-                                        .statusList[index].statusName!)
-                              ],
-                              stops: const [
-                                0.01,
-                                1,
-                              ]),
-                          borderGradient: const LinearGradient(
-                              colors: [Colors.transparent, Colors.transparent]),
-                          child: const Text("Bonjour"),
+                      return Container(
+                        margin: const EdgeInsets.all(20.0),
+                        padding: const EdgeInsets.all(16.0),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(
+                              10.0), // Border radius de 10
+                          gradient: LinearGradient(
+                            colors: [
+                              Colors.grey[900]!, // Couleur de fond sombre
+                              controller.getColorsByStatusTag(
+                                  statusTag: controller.statusList[index]
+                                      .statusName!), // Couleur de dégradé (bleu)
+                            ],
+                            begin: Alignment.topLeft,
+                            end: Alignment.bottomRight,
+                          ),
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.black.withOpacity(0.3),
+                              spreadRadius: 1,
+                              blurRadius: 5,
+                              offset: const Offset(3, 5), // Décalage de l'ombre
+                            ),
+                          ],
+                        ),
+                        child: const Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              'Training rocket league',
+                              style: TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white,
+                              ),
+                            ),
+                            SizedBox(height: 8.0),
+                            Text(
+                              'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+                              style: TextStyle(
+                                color: Colors.white70,
+                              ),
+                            ),
+                          ],
                         ),
                       );
                     }),
               ),
+              // Container(
+              //   margin: const EdgeInsets.all(20.0),
+              //   padding: const EdgeInsets.all(16.0),
+              //   decoration: BoxDecoration(
+              //     borderRadius:
+              //         BorderRadius.circular(10.0), // Border radius de 10
+              //     gradient: LinearGradient(
+              //       colors: [
+              //         Colors.grey[900]!, // Couleur de fond sombre
+              //         controller., // Couleur de dégradé (bleu)
+              //       ],
+              //       begin: Alignment.topLeft,
+              //       end: Alignment.bottomRight,
+              //     ),
+              //     boxShadow: [
+              //       BoxShadow(
+              //         color: Colors.black.withOpacity(0.3),
+              //         spreadRadius: 1,
+              //         blurRadius: 5,
+              //         offset: const Offset(3, 5), // Décalage de l'ombre
+              //       ),
+              //     ],
+              //   ),
+              //   child: const Column(
+              //     crossAxisAlignment: CrossAxisAlignment.start,
+              //     children: [
+              //       Text(
+              //         'Dark Theme Card',
+              //         style: TextStyle(
+              //           fontSize: 20,
+              //           fontWeight: FontWeight.bold,
+              //           color: Colors.white,
+              //         ),
+              //       ),
+              //       SizedBox(height: 8.0),
+              //       Text(
+              //         'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nonne merninisti licere mihi ista probare, quae sunt a te dicta? Refert tamen, quo modo.',
+              //         style: TextStyle(
+              //           color: Colors.white70,
+              //         ),
+              //       ),
+              //     ],
+              //   ),
+              // )
             ],
           ),
         ),
