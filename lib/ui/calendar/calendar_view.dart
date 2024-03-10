19d4e3a9c729bc9d11f821167532fdd7b3c07ebc
Dheeraj Tilhoo <abhi@Dheerajs-MacBook-Air.local>
2024-03-10 22:04:34 +0400

    ui complete calendar

--- lib/ui/calendar/calendar_view.dart
+++ lib/ui/calendar/calendar_view.dart
@@ -1,6 +1,7 @@
 import 'package:animated_bottom_navigation_bar/animated_bottom_navigation_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:stacktim_booking/helper/color.dart';
 import 'package:stacktim_booking/helper/strings.dart';
 import 'package:stacktim_booking/ui/calendar/calendar_view_controller.dart';
 import 'package:stacktim_booking/widget/x_app_bar.dart';
@@ -33,6 +34,35 @@ class CalendarPage extends GetView<CalendarViewController> {
             flex: 3,
             child: SfCalendar(
               view: CalendarView.month,
+              todayHighlightColor: Colors.transparent,
+              todayTextStyle: const TextStyle(color: Colors.white),
+              monthCellBuilder: (context, details) {
+                return Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    details.date.day.isEven
+                        ? Text(
+                            '${details.date.day}',
+                            style: const TextStyle(
+                              color: redStackTim,
+                            ),
+                          )
+                        : Text(
+                            '${details.date.day}',
+                            style: const TextStyle(
+                              color: Colors.white,
+                            ),
+                          ),
+                  ],
+                );
+              },
+              selectionDecoration: BoxDecoration(
+                color: Colors.transparent,
+                border: Border.all(
+                  color: Colors.transparent,
+                ),
+              ),
               cellBorderColor: Colors.transparent,
               monthViewSettings: MonthViewSettings(
                 dayFormat: 'EEE',
@@ -49,10 +79,89 @@ class CalendarPage extends GetView<CalendarViewController> {
               padding: const EdgeInsets.only(left: 18, right: 18, bottom: 18),
               child: Container(
                 decoration: BoxDecoration(
-                    color: Colors.grey,
+                    color: Colors.grey.withOpacity(0.5),
                     borderRadius: BorderRadius.circular(18)),
                 height: 300,
                 width: double.infinity,
+                child: const Padding(
+                  padding: EdgeInsets.all(12.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Légende',
+                        style: TextStyle(
+                          decoration: TextDecoration.underline,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Row(
+                        children: [
+                          Text(
+                            '27',
+                            style: TextStyle(
+                              color: redStackTim,
+                            ),
+                          ),
+                          SizedBox(
+                            width: 10,
+                          ),
+                          Text(
+                            'Session pleine',
+                            style: TextStyle(),
+                          ),
+                        ],
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Row(
+                        children: [
+                          Text(
+                            '23',
+                            style: TextStyle(
+                              color: blueChip,
+                            ),
+                          ),
+                          SizedBox(
+                            width: 10,
+                          ),
+                          Expanded(
+                            child: Text(
+                              'Quelques places encore disponibles',
+                              style: TextStyle(
+                                overflow: TextOverflow.ellipsis,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Row(
+                        children: [
+                          Text(
+                            '20',
+                          ),
+                          SizedBox(
+                            width: 10,
+                          ),
+                          Expanded(
+                            child: Text(
+                              'Toutes les places sont actuellement libres',
+                              style: TextStyle(
+                                overflow: TextOverflow.ellipsis,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
               ),
             ),
           )
