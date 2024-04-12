a00346c2c9d88d12734060905033f39c99e89cdb
j.seux@xefi.fr <j.seux@xefi.fr>
2024-04-12 15:39:57 +0200

    📦️ compress assets 📦️

--- lib/helper/strings.dart
+++ lib/helper/strings.dart
@@ -1,9 +1,9 @@
 //STUB - FOLDER BACKGROUND
 const String logo = 'assets/background/logo.png';
-const String one = 'assets/background/1.jpg';
+const String one = 'assets/background/1.png';
 const String two = 'assets/background/2.jpg';
 const String three = 'assets/background/3.png';
-const String four = 'assets/background/4.jpg';
+const String four = 'assets/background/4.png';
 const String five = 'assets/background/5.jpg';
 const String six = 'assets/background/6.png';
 const String microsoftLogo = 'assets/background/microsoft.png';
