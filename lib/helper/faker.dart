b8f3ff5cc4b602a1965cf66c58dd1c79629e998f
Julien SEUX <j.seux@xefi.fr>
2024-07-11 13:29:57 +0200

    ⚡️ change hour of faker ⚡️

--- lib/helper/faker.dart
+++ lib/helper/faker.dart
@@ -1,5 +1,5 @@
 bool isProdDate() {
-  DateTime prodDay = DateTime(2024, 7, 11, 12);
+  DateTime prodDay = DateTime(2024, 7, 11, 14, 30);
   if (DateTime.now().isBefore(prodDay)) {
     return false;
   } else {
